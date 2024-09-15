2822b29233487401fb98f073bb9c5cd5ec6ba080
gunjan patel <g.a.patel359@gmail.com>
2024-09-15 18:15:03 +0530

    working on it

--- app/src/main/java/com/example/assignment/HomeActivity.kt
+++ app/src/main/java/com/example/assignment/HomeActivity.kt
@@ -1,34 +1,27 @@
 package com.example.assignment
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        findViewById<Button>(R.id.loginbtn).setOnClickListener {
-            Intent(this,LoginActivity::class.java).also {
-                startActivity(it)
-            }
-        }
-
-        findViewById<Button>(R.id.registerbtn).setOnClickListener {
-            Intent(this,RegisterActivity::class.java).also {
-                startActivity(it)
-            }
-        }
+        val retrofitBuilder=Retrofit.Builder()
+            .baseUrl("https://deezerdevs-deezer.p.rapidapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
     }
 }
