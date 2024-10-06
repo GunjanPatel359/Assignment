840258aa14d60fa269c08e869c04fcdce0de6b65
gunjan patel <g.a.patel359@gmail.com>
2024-10-06 21:20:17 +0530

    com.google.android.material.bottomnavigation.BottomNavigationView bottom whitespace error resolve by removing         enableEdgeToEdge() from MainActivity.kt

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -15,7 +15,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
